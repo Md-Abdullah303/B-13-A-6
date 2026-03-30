d4f8ec0678c6cd074293154cbea60bf55dda12b4
Khan Mohammad Abdullah <mdabdulla01715940008@gmail.com>
2026-03-30 18:12:30 +0600

    Install all React package also install tailwind css, DaisyUI, react icon, react toastify and chack are working perfectly, i added all assets file that will be need

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { BiFlag } from 'react-icons/bi'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='btn btn-success text-red-500 text-3xl'>hi <BiFlag></BiFlag></h1>
+      <button className='btn btn-error' >hi</button>
+      
+    </>
+  )
+}
+
+export default App
